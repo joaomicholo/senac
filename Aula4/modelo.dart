0037246f56fe775d684e7a780735990c3f385509
Joao Gabriel "Micholo <51175419+joaomicholo@users.noreply.github.com>
2019-06-19 15:45:09 -0300

    Create modelo.dart

---
+++ Aula4/modelo.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+//Método principal
+void main(){
+  runApp(MyApp());
+}
+
+//Classe MyApp
+class MyApp extends StatefulWidget {
+   @override
+  State<StatefulWidget> createState (){
+     return _MyAppState();
+   }
+
+  }
+
+//Classe MyappState que herda (extends)a classe State
+class _MyAppState extends State<MyApp> {
+
+
+  //Interface ######################################
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //title: "Exemplo de Classes e Objetos",
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Classes e Objetos"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
